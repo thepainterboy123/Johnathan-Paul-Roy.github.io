955ba927565ee3552481654f2e163662d19042f6
Johnathan Paul Roy <thepainterboy123@gmail.com>
2022-01-02 21:58:28 -0700

    Update contactform.php

--- php/contactform.php
+++ php/contactform.php
@@ -6,7 +6,7 @@ error_reporting(0);
   
  // configuration
  
-$email_it_to = "your_own_email_address@some_domain.com";
+$email_it_to = "thepainterboy123@gmail.com";
 
 $error_message = "Please complete the form first";
 
